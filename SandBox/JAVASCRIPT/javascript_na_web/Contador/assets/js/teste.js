d69e676e3ac8d752d3d22b8a047ba0eb835ebb1e
Alexandre Filho <45580663+AlexandreMFilho@users.noreply.github.com>
2022-07-21 21:53:55 -0300

    Adicionado limites contador

--- SandBox/JAVASCRIPT/javascript_na_web/Contador/assets/js/teste.js
+++ SandBox/JAVASCRIPT/javascript_na_web/Contador/assets/js/teste.js
@@ -1,16 +1,27 @@
 let contador = 0 ;
+let minimo = -100, maximo = 100;
 
 const CURRENT_NUMBER = document.getElementById('valor');
 
+function atualiza(){
+
+    if(minimo < maximo){
+        minimo = document.getElementsByClassName("floor")[0].value;
+        maximo = document.getElementsByClassName("ceil")[0].value
+    }else{
+        console.log("O valor mínimo deve ser menor que o máximo.");
+    }
+}
+
 function incrementa(){
-    if(contador < 10){
+    if(contador < maximo){
         contador++;
         CURRENT_NUMBER.innerHTML = contador;
     }
     positivo();
 }
 function decrementa(){
-    if(contador > -10){
+    if(contador > minimo){
         contador--;
         CURRENT_NUMBER.innerHTML = contador;
     }
